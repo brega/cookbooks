46a8a0930d3e5159799de66c6756cfdd57a0c6e3
Bregadeesh Samapathy <brega@luxola.com>
2013-04-06 16:57:17 +0800

    Add recipe for registering Opsworks instances to ELB

---
+++ aws/recipes/deregister.rb
@@ -0,0 +1,6 @@
+include_recipe 'aws'
+
+execute 'deregister' do
+  command "aws --region #{node[:opsworks][:instance][:region] elb deregister-instances-from-load-balancer --load-balancer-name lxtest-elb --instances '{\"instance_id\":\"#{node[:opsworks][:instance][:aws_instance_id]}\"}'"
+  user 'deploy'
+end
---
+++ aws/recipes/register.rb
@@ -0,0 +1,6 @@
+include_recipe 'aws'
+
+execute 'register' do
+  command "aws --region #{node[:opsworks][:instance][:region]} elb register-instances-with-load-balancer --load-balancer-name lxtest-elb --instances {\"instance_id\": \"#{node[:opsworks][:instance][:aws_instance_id]}\"}'"
+  user 'deploy'
+end
