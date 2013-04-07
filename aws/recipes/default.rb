550df0a063a5134d0eb645eee04049475e7f74e6
Bregadeesh Samapathy <brega@luxola.com>
2013-04-07 23:15:48 +0800

    Remove printing env variable

--- aws/recipes/default.rb
+++ aws/recipes/default.rb
@@ -1,13 +1,6 @@
 ENV['AWS_ACCESS_KEY_ID'] = node[:aws_access][:access_key]
 ENV['AWS_SECRET_ACCESS_KEY'] = node[:aws_access][:secret_key]
 
-bash "env_test" do
-  code<<-EOF
-  echo $AWS_ACCESS_KEY_ID
-  echo $AWS_SECRET_ACCESS_KEY
-EOF
-end
-
 package 'python-pip' do
   action :install
 end
